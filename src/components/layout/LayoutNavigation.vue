1ea8044af94dce5a04d6cc73ee26638479e152de
Guilherme <gb-ol@hotmail.com>
2019-05-29 16:40:23 -0300

    logout

--- src/components/layout/LayoutNavigation.vue
+++ src/components/layout/LayoutNavigation.vue
@@ -1,26 +1,33 @@
 <template>
-  <nav class="nav flex-column">
-    <router-link
-      v-for="(router, i) in routerLinks"
-      :key="i"
-      class="nav-link"
-      exact-active-class="active"
-      :to="{ name: router.name }"
-    >
-      <i class="fa" :class="`fa-${router.meta.icon}`"></i>
-      {{ router.meta.title }}
-    </router-link>
+  <nav class="nav flex-column justify-content-between">
+    <div>
+      <router-link
+        v-for="(router, i) in routerLinks"
+        :key="i"
+        class="nav-link"
+        exact-active-class="active"
+        :to="{ name: router.name }"
+      >
+        <i class="fa" :class="`fa-${router.meta.icon}`"></i>
+        {{ router.meta.title }}
+      </router-link>
+    </div>
+    <layout-logout/>
   </nav>
 </template>
 
 <script>
+import LayoutLogout from './LayoutLogout'
 export default {
+  components: {
+    LayoutLogout
+  },
   computed: {
-    routerLinks() {
-      return this.$router.options.routes.filter(r => r.name !== "login");
+    routerLinks () {
+      return this.$router.options.routes.filter(r => r.name !== 'login')
     }
   }
-};
+}
 </script>
 
 <style scoped lang="scss">
