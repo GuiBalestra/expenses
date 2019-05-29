1376aa86af1cb3f32551f48e0289e5bb080c8419
Guilherme <gb-ol@hotmail.com>
2019-05-29 10:08:52 -0300

    autenticando usuario no firebase

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,24 +1,43 @@
 <template>
-  <form class="form-login">
+  <form class="form-login" @submit.prevent="doLogin()">
     <div class="card">
       <div class="card-header text-center">
         <h1 class="mb-0">Expenses</h1>
       </div>
       <div class="card-body">
         <div class="form-group">
-          <input type="text" class="form-control" placeholder="E-mail">
+          <input type="email" class="form-control" placeholder="E-mail" required v-model="email">
         </div>
         <div class="form-group">
-          <input type="password" class="form-control" placeholder="Senha">
+          <input type="password" class="form-control" placeholder="Senha" required v-model="password">
         </div>
-        <button class="btn btn-primary w-100">Enviar</button>
+        <button class="btn btn-primary w-100">Entrar</button>
       </div>
     </div>
   </form>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'Login',
+  data: () => {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async doLogin () {
+      const {email, password} = this
+      try {
+        const res = await this.$firebase.auth().signInWithEmailAndPassword(email, password)
+        console.log(res)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -27,8 +46,11 @@ export default {};
   display: flex;
   align-items: center;
   justify-content: center;
+  h1 {
+    font-size: 18pt;
+  }
   .card {
-    width: 25%;
+    width: 30%;
     color: var(--darker);
   }
 }
